38e0133ed8976a2d8c19acac1d7bd840d110c4b6
cristian sanchez brandan <csanchezbrandan@gmail.com>
2023-06-21 17:39:14 -0300

    primeros cambios en pagina 404

---
+++ notFoungPage/src/App.jsx
@@ -0,0 +1,15 @@
+import { Component, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import notFoundImg from './assets/Scarecrow'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <notFoundImg/>
+    </>
+  )
+}
+
+export default App
